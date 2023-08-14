d9d5c28e3c51c9eee8e66036ebd3c2427e49538d
XJF <785321818@qq.com>
2023-08-14 21:12:10 +0800

    Initial commit

--- test.py
+++ test.py
@@ -1,5 +1,1 @@
-from magent2.environments import battle_v4
-from pettingzoo.utils import random_demo
-
-env = battle_v4.env(render_mode='human')
-random_demo(env, render=True, episodes=1000)
+print("WTF!")
